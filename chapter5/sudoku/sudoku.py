95de0a1d4f85a865bd0b029cc2fbd8ec6ce08ab9
Ridwan Nurhayat <ridwan.nurhayat@traveloka.com>
2020-06-01 12:31:22 +0700

    Convert tab to space

--- chapter5/sudoku/sudoku.py
+++ chapter5/sudoku/sudoku.py
@@ -1,22 +1,22 @@
 def checkBoard(arr):
-	for a in range(3):
-		for b in range(3):
-			temp = set()
-			for c in range(3):
-				for d in range(3):
-					num = lst[c+a*3][d+b*3]
-					if num in temp:
-						return False
-					else:
-						temp.add(num)
-	return True
+    for a in range(3):
+        for b in range(3):
+            temp = set()
+            for c in range(3):
+                for d in range(3):
+                    num = lst[c+a*3][d+b*3]
+                    if num in temp:
+                        return False
+                    else:
+                        temp.add(num)
+    return True
 
 print("Enter numbers:")
 lst = []
 for _ in range(9):
-	lst.append(list(map(int, list(input().strip()))))
+    lst.append(list(map(int, list(input().strip()))))
 if checkBoard(lst):
-	print("Yes")
+    print("Yes")
 else:
-	print("No")
-	
+    print("No")
+    
